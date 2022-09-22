7056e20c772332875fcc4b9c3ab06d3fc5730883
Spike <l-yanlei@hotmail.com>
2022-09-22 16:52:29 +0800

    fix: remove no used code

--- config.js
+++ config.js
@@ -4,7 +4,6 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // update to your resume.md path
 export const resumePath = path.resolve(__dirname, "./src/resume.md")
-// export const resumePath = "/Users/spike/Dropbox/resume/resume.md"
 
 export const distPath = path.resolve(__dirname, "./dist", "resume.html")
 export const stylePath = path.resolve(__dirname, "./src/style/index.css")
